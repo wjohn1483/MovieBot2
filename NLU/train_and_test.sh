052d0745f6bbb055bbe5e7bc82123c3cfcb31228
Roy <b01501061@gmail.com>
2017-06-17 00:42:53 +0800

    add CUDA_VISIBLE_DEVICES when training NLU

--- NLU/train_and_test.sh
+++ NLU/train_and_test.sh
@@ -1,2 +1,11 @@
+#! /bin/bash -xe
+if [ $# -ne 1 ]; then
+    echo "Usage:"
+    echo "    bash $0 <device number>"
+    echo "E.g."
+    echo "    bash $0 0"
+    exit 1
+fi
+export CUDA_VISIBLE_DEVICES=$1 
 (time python3 ./NLU/codes/nlu_slot_filling_and_intent_prediction.py --train True) 2>&1 | tee ./NLU/log_train
 (time python3 ./NLU/codes/nlu_slot_filling_and_intent_prediction.py --train False) 2>&1 | tee ./NLU/log_test
