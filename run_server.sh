a2a18f288c6ce170e7605e85977769abc8b38c48
wjohn1483 <wjohn1483@yahoo.com.tw>
2017-06-16 17:32:52 +0800

    Modified for dependency and model files

--- run_server.sh
+++ run_server.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-CUDA_VISIBLE_DEVICES=1 python3.5 server.py
+
+if [ ! -d "model_acc48" ]; then
+    wget --no-check-certificate "https://speech.ee.ntu.edu.tw/~wjohn1483/MovieBot/model_acc48.zip"
+    unzip model_acc48.zip
+    rm model_acc48.zip
+fi
+
+CUDA_VISIBLE_DEVICES=1 python3 server.py
